d6d2fd31ea978bac1eff9c48fe5c5ff93f0de0e2
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-05-16 03:55:20 +0400

    Private Properties and Methods.

--- index_objects.js
+++ index_objects.js
@@ -21,3 +21,27 @@ function Circle(radius) {
 const circle = new Circle(10);
 circle.computeOptimumLocation(); // this can put our object in a bad state
 circle.draw(); // then here we can get a wierd error in runtime
+
+
+//-----------Private Properties and Methods-----------
+function CircleP(radius) {
+    this.radius = radius;
+
+    let defaultLocation = { x: 0, y: 0 }; // private
+
+    let computeOptimumLocation = function(factor) { // private
+        // ...
+    }
+
+    this.draw = function() {
+        //let x, y;
+        computeOptimumLocation(0.1);
+        // defaultLocation
+        // this.radius
+
+        console.log('draw');
+    };
+}
+
+const circleP = new CircleP(10);
+circleP.draw(); // draw
