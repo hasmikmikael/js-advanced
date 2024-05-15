76cbc8f97080c4e530cf09a14c455d3285ee4ab8
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-05-16 02:19:19 +0400

    Abstraction.

---
+++ index_objects.js
@@ -0,0 +1,23 @@
+//-------Abstraction-----------
+
+// In this example we have isses
+// we should make defaultLocation and computeOptimumLocation private
+function Circle(radius) {
+    this.radius = radius;
+
+    this.defaultLocation = { x: 0, y: 0 }; // this should be private
+
+    this.computeOptimumLocation = function() { // this should be private
+        // ...
+    }
+
+    this.draw = function() {
+        this.computeOptimumLocation();
+
+        console.log('draw');
+    };
+}
+
+const circle = new Circle(10);
+circle.computeOptimumLocation(); // this can put our object in a bad state
+circle.draw(); // then here we can get a wierd error in runtime
