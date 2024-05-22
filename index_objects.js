90b00c6467c92c78515018ad757e25650b8cb656
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-05-22 17:18:28 +0400

    Getters and Setters.

--- index_objects.js
+++ index_objects.js
@@ -45,3 +45,48 @@ function CircleP(radius) {
 
 const circleP = new CircleP(10);
 circleP.draw(); // draw
+
+
+//---------Getters and Setters-------------
+//this is not a preferable solution
+function CircleG(radius) {
+    this.radius = radius;
+
+    let defaultLocation = { x: 0, y: 0 };
+
+    this.getDefaultLocation = function() {
+        return defaultLocation;
+    }
+
+    this.draw = function() {
+        console.log('draw');
+    };
+}
+
+const circleG = new CircleG(10);
+circleG.getDefaultLocation();
+circleG.draw(); // draw
+
+//this is better solution
+function CircleGS(radius) {
+    this.radius = radius;
+
+    let defaultLocation = { x: 0, y: 0 };
+
+    Object.defineProperty(this, 'defaultLocation', {
+        get: function() {
+            return defaultLocation;
+        },
+        set: function(value) {
+            if (!value.x || !value.y)
+              throw new Error('Invalid location');
+
+              defaultLocation = value;
+        }
+    });
+}
+
+const circleGS = new CircleGS(10);
+circleGS.defaultLocation;
+//circleGS.defaultLocation = 1;
+console.log(circleGS);
