cb29e0e0db275bdb06be42328f3ee65559f1a003
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-05-26 02:26:55 +0400

    Calling the Super Constructor.

--- index_prototypical_inheritance.js
+++ index_prototypical_inheritance.js
@@ -46,3 +46,30 @@ CircleR.prototype.draw = function() {
 
 const sp = new ShapeR();
 const cl = new CircleR(1);
+
+
+//-------Calling the Super Constructor--------
+function Shape(color) {
+    this.color = color;
+}
+
+Shape.prototype.duplicate = function() {
+    console.log('duplicate');
+}
+
+function Circle(radius, color) {
+    // Super Constructor
+    Shape.call(this, color);
+
+    this.radius = radius;
+}
+
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.draw = function() {
+    console.log('draw');
+}
+
+const shape = new Shape();
+const circle = new Circle(1, 'red'); // Circle {color: 'red', radius: 1}
